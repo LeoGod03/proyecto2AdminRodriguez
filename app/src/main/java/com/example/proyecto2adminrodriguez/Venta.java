b7cc1aed65f627d0100d8fa30d7e4c8f9c4dd052
leoGod03 <leopardo03.2001@outlook.com>
2025-03-04 17:27:51 -0600

    Implementación producto

--- app/src/main/java/com/example/proyecto2adminrodriguez/Venta.java
+++ app/src/main/java/com/example/proyecto2adminrodriguez/Venta.java
@@ -1,14 +1,35 @@
 package com.example.proyecto2adminrodriguez;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Venta extends AppCompatActivity {
-
+public class Venta extends AppCompatActivity { // empieza clase
+    public EditText etProducto, etCantidad, etPrecio;
+    public TextView tvResultadoVenta;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {// empieza onCreate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venta);
+        // integración de XML a Java
+        etProducto = findViewById(R.id.etProducto);
+        etCantidad =  findViewById(R.id.etCantidad);
+        etPrecio = findViewById(R.id.etPrecio);
 
-    }
-}
+        tvResultadoVenta = findViewById(R.id.tvResultadoVenta);
+
+    } // termina onCreate
+
+    public void venta(View view){ // inicia método
+        String nombreProducto = etProducto.getText().toString();
+        int cantidadProducto = Integer.parseInt(etCantidad.getText().toString());
+        float precioProducto = Float.parseFloat(etPrecio.getText().toString());
+
+        float ventaTotal = cantidadProducto * precioProducto;
+        tvResultadoVenta.setText(String.valueOf(ventaTotal));
+    } // termina método
+}// termina clase
